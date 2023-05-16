172ce96526352f6ddb8a752778fdb7f8b11b22b5
Mike DeLong <mikedelong@outlook.com>
2023-05-16 12:24:51 -0400

    feature: first working version to make and save the iris pairplot

--- iris.py
+++ iris.py
@@ -2,7 +2,9 @@ from logging import INFO
 from logging import basicConfig
 from logging import getLogger
 
+from matplotlib.pyplot import savefig
 from pandas import DataFrame
+from seaborn import pairplot
 from sklearn.datasets import load_iris
 
 
@@ -14,11 +16,19 @@ def get_data() -> DataFrame:
 
 
 def main():
-    basicConfig(level=INFO, )
+    basicConfig(level=INFO, datefmt='%Y-%m-%d %H:%M:%S',
+                format='%(asctime)s.%(msecs)03d - %(levelname)s - %(name)s - %(message)s', )
     logger = getLogger(name='main', )
     data_df = get_data()
-    logger.info(data_df.columns.tolist())
+    logger.info(msg='columns: %s'.format(data_df.columns.tolist()))
+    pairplot(data=data_df, hue='target')
+    filename = OUTPUT_FOLDER + OUTPUT_FILE
+    logger.info(msg='saving plot to {}'.format(filename))
+    savefig(format='png', fname=filename)
 
+
+OUTPUT_FOLDER = './result/'
+OUTPUT_FILE = 'iris.pairplot.png'
 
 if __name__ == '__main__':
     main()
