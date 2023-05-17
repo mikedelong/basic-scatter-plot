e165cf26d11a53899afc033a5f8880bb69a99b04
Mike DeLong <mikedelong@outlook.com>
2023-05-17 10:47:17 -0400

    feature: a second version that includes somewhat cluttered labels

--- world_happiness_report.py
+++ world_happiness_report.py
@@ -3,6 +3,10 @@ from logging import basicConfig
 from logging import getLogger
 
 from matplotlib.pyplot import savefig
+from matplotlib.pyplot import text
+from matplotlib.pyplot import title
+from matplotlib.pyplot import xlabel
+from matplotlib.pyplot import ylabel
 from pandas import DataFrame
 from pandas import read_excel
 from seaborn import scatterplot
@@ -18,7 +22,12 @@ def main():
                 format='%(asctime)s.%(msecs)03d - %(levelname)s - %(name)s - %(message)s', )
     logger = getLogger(name='main', )
     data_df = get_data(url=FIGURE_URL)
-    scatterplot(data=data_df, x='RANK', y='Happiness score', )
+    scatterplot(data=data_df, x='RANK', y='Happiness score', markers='')
+    title('Happiness Score (2022)')
+    xlabel('Rank')
+    ylabel('Score')
+    for index, (rank, score) in enumerate(zip(data_df['RANK'].values, data_df['Happiness score'].values)):
+        text(x=rank, y=score, s=data_df['Country'].values[index], ha='left', fontsize=5)
     filename = OUTPUT_FOLDER + OUTPUT_FILE
     logger.info(msg='saving plot to {}'.format(filename))
     savefig(backend=None, bbox_inches=None, dpi='figure', edgecolor='auto', facecolor='auto', fname=filename,
