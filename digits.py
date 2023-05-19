8efd3afd4c39e19c9f3a91bcde3ff9ced9706eb8
Mike DeLong <mikedelong@outlook.com>
2023-05-19 11:23:32 -0400

    feature: introduce the t-SNE model

--- digits.py
+++ digits.py
@@ -9,6 +9,7 @@ from pandas import DataFrame
 from seaborn import scatterplot
 from sklearn.datasets import load_digits
 from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
 
 def get_data() -> DataFrame:
@@ -17,20 +18,22 @@ def get_data() -> DataFrame:
     return result_df
 
 
-def make_pca_scatter(df: DataFrame):
+def make_scatter(df: DataFrame):
     logger = getLogger(name='make_pca_scatter', )
     logger.info(msg='columns: {}'.format(df.columns.tolist()))
-    model = PCA(n_components=2, )
-    pixel_columns = [column for column in df.columns if column.startswith('pixel')]
-    projection = model.fit_transform(X=df[pixel_columns])
-    projection_df = DataFrame(data=projection, columns=['x', 'y'])
-    projection_df['class'] = df['class']
-    scatterplot(data=projection_df, hue='class', palette='colorblind', x='x', y='y',  )
-    filename = OUTPUT_FOLDER + OUTPUT_FILE
-    logger.info(msg='saving plot to {}'.format(filename))
-    savefig(backend=None, bbox_inches=None, dpi='figure', edgecolor='auto', facecolor='auto', fname=filename,
-            format='png', metadata=None, pad_inches=0.1, )
-    close()
+    for (model, filename) in [
+        (PCA(n_components=2, ), OUTPUT_FOLDER + SCATTER_PCA_FILE,),
+        (TSNE(n_components=2, random_state=RANDOM_STATE, ), OUTPUT_FOLDER + SCATTER_TSNE_FILE,)
+    ]:
+        pixel_columns = [column for column in df.columns if column.startswith('pixel')]
+        projection = model.fit_transform(X=df[pixel_columns])
+        projection_df = DataFrame(data=projection, columns=['x', 'y'])
+        projection_df['class'] = df['class']
+        scatterplot(data=projection_df, hue='class', legend=False, palette='colorblind', x='x', y='y', )
+        logger.info(msg='saving plot to {}'.format(filename))
+        savefig(backend=None, bbox_inches=None, dpi='figure', edgecolor='auto', facecolor='auto', fname=filename,
+                format='png', metadata=None, pad_inches=0.1, )
+        close()
 
 
 def main():
@@ -39,13 +42,15 @@ def main():
                 format='%(asctime)s.%(msecs)03d - %(levelname)s - %(name)s - %(message)s', )
     logger = getLogger(name='main', )
     data_df = get_data()
-    make_pca_scatter(df=data_df, )
+    make_scatter(df=data_df, )
     time_seconds = (now() - time_start).total_seconds()
     logger.info(msg='done: {:02d}:{:05.2f}'.format(int(time_seconds // 60), time_seconds % 60))
 
 
 OUTPUT_FOLDER = './result/'
-OUTPUT_FILE = 'digits.scatterplot.png'
+RANDOM_STATE = 0
+SCATTER_PCA_FILE = 'digits.pca.scatterplot.png'
+SCATTER_TSNE_FILE = 'digits.tsne.scatterplot.png'
 
 if __name__ == '__main__':
     main()
