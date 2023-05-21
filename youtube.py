48c8877ca27b6012aedc5ced527ce1b0c785f79d
Mike DeLong <mikedelong@outlook.com>
2023-05-21 08:37:59 -0400

    feature: boilerplate code with logging and total time

---
+++ youtube.py
@@ -0,0 +1,19 @@
+from logging import INFO
+from logging import basicConfig
+from logging import getLogger
+
+from arrow import now
+
+
+def main():
+    time_start = now()
+    basicConfig(level=INFO, datefmt='%Y-%m-%d %H:%M:%S',
+                format='%(asctime)s.%(msecs)03d - %(levelname)s - %(name)s - %(message)s', )
+    logger = getLogger(name='main', )
+    time_seconds = (now() - time_start).total_seconds()
+    logger.info(msg='done: {:02d}:{:05.2f}'.format(int(time_seconds // 60), time_seconds % 60))
+
+
+DEBUG = {}
+if __name__ == '__main__':
+    main()
