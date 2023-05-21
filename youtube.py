9c8ef52bc4b5b4b4dfed9bd794aa71419e6abf65
Mike DeLong <mikedelong@outlook.com>
2023-05-21 09:08:35 -0400

    feature: first working version to get a channel and get the details for a video

--- youtube.py
+++ youtube.py
@@ -3,6 +3,7 @@ from logging import basicConfig
 from logging import getLogger
 
 from arrow import now
+from scrapetube import get_channel
 
 
 def main():
@@ -10,6 +11,14 @@ def main():
     basicConfig(level=INFO, datefmt='%Y-%m-%d %H:%M:%S',
                 format='%(asctime)s.%(msecs)03d - %(levelname)s - %(name)s - %(message)s', )
     logger = getLogger(name='main', )
+
+    videos = get_channel('UCCezIgC97PvUuR4_gbFUs5g')
+
+    for video in list(videos)[:2]:
+        # logger.info(video['videoId'])
+        logger.info(video)
+        DEBUG['video'] = video
+
     time_seconds = (now() - time_start).total_seconds()
     logger.info(msg='done: {:02d}:{:05.2f}'.format(int(time_seconds // 60), time_seconds % 60))
 
