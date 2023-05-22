749a91942d17f8e769890eda6e86df30ce273c4b
Mike DeLong <mikedelong@outlook.com>
2023-05-22 13:44:08 -0400

    feature: first working version of code that turns a description of a period of time into a date

--- youtube.py
+++ youtube.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from json import load
 from logging import INFO
 from logging import basicConfig
@@ -7,6 +8,21 @@ from re import findall
 from arrow import now
 from pandas import DataFrame
 from scrapetube import get_channel
+
+
+def get_timedelta(adjustment: str):
+    pieces = adjustment.split()
+    amount = -int(pieces[0])
+    if pieces[1] == 'hours':
+        return timedelta(hours=amount)
+    elif pieces[1] == 'days':
+        return timedelta(days=amount)
+    elif pieces[1] == 'months':
+        return timedelta(days=30 * amount)
+    elif pieces[1] == 'years':
+        return timedelta(days=365 * amount)
+    else:
+        return timedelta(days=0)
 
 
 def main():
@@ -32,6 +48,7 @@ def main():
     videos_df = DataFrame(data={'id': video_ids, 'published_time_text': published_time, 'view_count_text': view_count})
     DEBUG['video_data'] = videos_df
     videos_df['view_count'] = videos_df['view_count_text'].astype(str).apply(lambda x: int(''.join(findall(r'\d+', x))))
+    videos_df['published_date'] = videos_df['published_time_text'].apply(get_timedelta) + time_start.datetime
 
     time_seconds = (now() - time_start).total_seconds()
     logger.info(msg='done: {:02d}:{:05.2f}'.format(int(time_seconds // 60), time_seconds % 60))
