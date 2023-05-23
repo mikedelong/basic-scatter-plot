013771b68eb13f1bcca323590e2d99162b46d6a3
Mike DeLong <mikedelong@outlook.com>
2023-05-23 14:03:39 -0400

    feature: plot using both matplotlib and seaborn, in a minimal amount of code

--- youtube_load_and_plot.py
+++ youtube_load_and_plot.py
@@ -7,9 +7,10 @@ from arrow import now
 from matplotlib.pyplot import close
 from matplotlib.pyplot import savefig
 from matplotlib.pyplot import scatter
+from matplotlib.pyplot import ylabel
 from pandas import read_csv
 from pandas import to_datetime
-from matplotlib.pyplot import ylabel
+from seaborn import scatterplot
 
 
 def main():
@@ -22,21 +23,24 @@ def main():
     videos_df['log10_views'] = videos_df['views'].apply(math.log10)
     videos_df['published'] = to_datetime(arg=videos_df['published'], )
     videos_df = videos_df.sort_values(by='published', )
-    scatter(data=videos_df, x='published', y='log10_views', )
-    ylabel(ylabel='log10 views')
-    filename = OUTPUT_FOLDER + FILENAME
-    logger.info(msg='saving plot to {}'.format(filename), )
-    savefig(backend=None, bbox_inches=None, dpi='figure', edgecolor='auto', facecolor='auto', fname=filename,
-            format='png', metadata=None, pad_inches=0.1, )
-    close()
+    for (plot_function, filename) in [
+        (scatter, OUTPUT_FOLDER + SCATTER_FILENAME),
+        (scatterplot, OUTPUT_FOLDER + SCATTERPLOT_FILENAME)
+    ]:
+        plot_function(data=videos_df, x='published', y='log10_views', )
+        ylabel(ylabel='log10 views')
+        logger.info(msg='saving plot to {}'.format(filename), )
+        savefig(backend=None, bbox_inches=None, dpi='figure', edgecolor='auto', facecolor='auto', fname=filename,
+                format='png', metadata=None, pad_inches=0.1, )
+        close()
 
     time_seconds = (now() - time_start).total_seconds()
     logger.info(msg='done: {:02d}:{:05.2f}'.format(int(time_seconds // 60), time_seconds % 60, ))
 
 
-DEBUG = {}
-FILENAME = 'youtube.matplotlib.scatter.png'
 OUTPUT_FOLDER = './result/'
+SCATTER_FILENAME = 'youtube.matplotlib.scatter.png'
+SCATTERPLOT_FILENAME = 'youtube.seaborn.scatterplot.png'
 
 if __name__ == '__main__':
     main()
