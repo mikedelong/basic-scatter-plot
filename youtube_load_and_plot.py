3309890fff4e95916e923b98fbcd1a2b56a2366c
Mike DeLong <mikedelong@outlook.com>
2023-05-23 09:20:48 -0400

    feature: first working version to produce something that looks reasonable

--- youtube_load_and_plot.py
+++ youtube_load_and_plot.py
@@ -7,6 +7,7 @@ from matplotlib.pyplot import close
 from matplotlib.pyplot import savefig
 from matplotlib.pyplot import scatter
 from pandas import read_csv
+from pandas import to_datetime
 
 
 def main():
@@ -16,16 +17,17 @@ def main():
     logger = getLogger(name='main', )
 
     videos_df = read_csv(filepath_or_buffer='./videos.csv', usecols=['published', 'views'])
-    videos_df = videos_df.sort_values(by='published')
-    scatter(data=videos_df, x='published', y='views')
+    videos_df['published'] = to_datetime(arg=videos_df['published'], )
+    videos_df = videos_df.sort_values(by='published', )
+    scatter(data=videos_df, x='published', y='views', )
     filename = OUTPUT_FOLDER + FILENAME
-    logger.info(msg='saving plot to {}'.format(filename))
+    logger.info(msg='saving plot to {}'.format(filename), )
     savefig(backend=None, bbox_inches=None, dpi='figure', edgecolor='auto', facecolor='auto', fname=filename,
             format='png', metadata=None, pad_inches=0.1, )
     close()
 
     time_seconds = (now() - time_start).total_seconds()
-    logger.info(msg='done: {:02d}:{:05.2f}'.format(int(time_seconds // 60), time_seconds % 60))
+    logger.info(msg='done: {:02d}:{:05.2f}'.format(int(time_seconds // 60), time_seconds % 60, ))
 
 
 DEBUG = {}
