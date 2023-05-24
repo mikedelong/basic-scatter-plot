a7e456910ff99a0fc607c42d1652d0c4ccefc447
Mike DeLong <mikedelong@outlook.com>
2023-05-24 12:50:17 -0400

    feature: first working version to correctly use the generator we get from scrapetube; still needs work

--- youtube.py
+++ youtube.py
@@ -1,20 +1,20 @@
-from datetime import timedelta
+from datetime import datetime
 from json import load
 from logging import INFO
 from logging import basicConfig
 from logging import getLogger
-from re import findall
 from typing import Generator
 
 from arrow import now
+from bs4 import BeautifulSoup as bs
 from matplotlib.pyplot import close
 from matplotlib.pyplot import savefig
 from pandas import DataFrame
+from pandas import to_datetime
+from pyppeteer.errors import TimeoutError
+from requests_html import HTMLSession
 from scrapetube import get_channel
 from seaborn import scatterplot
-
-from requests_html import HTMLSession
-from bs4 import BeautifulSoup as bs # importing BeautifulSoup
 
 
 def get_generator(channel: str, channel_kind: str, ) -> Generator:
@@ -26,44 +26,36 @@ def get_generator(channel: str, channel_kind: str, ) -> Generator:
         raise NotImplementedError(channel_kind)
 
 
-def alt(videos: Generator) -> DataFrame:
-    return DataFrame()
-
+# todo get other columns of data including name, upload date, video ID, etc.
+# todo figure out how to encode the channel name
+# todo report both the view count and the log thereof
 
 def get_data_from_generator(videos: Generator) -> DataFrame:
-    arrow_now = now()
-    logger = getLogger(name='get_data_from_url')
-
+    logger = getLogger(name='get_data_from_generator')
     video_ids = []
-    published_time = []
-    view_count = []
+    views = []
+    date_published = []
     for video in videos:
         video_id = video['videoId']
-        video_ids.append(video_id)
-        published_time.append(video['publishedTimeText']['simpleText'])
-        view_count.append(video['viewCountText']['simpleText'])
-        logger.info(video_id)
-        DEBUG['example_video'] = video
-    result_df = DataFrame(data={'id': video_ids, 'published_time_text': published_time, 'view_count_text': view_count})
-    DEBUG['video_data'] = result_df
-    result_df['view_count'] = result_df['view_count_text'].astype(str).apply(lambda x: int(''.join(findall(r'\d+', x))))
-    result_df['published_date'] = result_df['published_time_text'].apply(get_timedelta) + arrow_now.datetime
+        url = 'https://youtu.be/{}'.format(video_id)
+        logger.info(msg=url, )
+        try:
+            with HTMLSession() as session:
+                response = session.get(url=url, )
+                response.html.render(sleep=1)
+                soup = bs(response.html.html, 'html.parser')
+                video_ids.append(video_id)
+                views.append(soup.find('meta', itemprop='interactionCount')['content'])
+                date_published.append(soup.find('meta', itemprop='datePublished')['content'])
+        except TimeoutError as timeout_error:
+            logger.warning(timeout_error)
+
+    date_published = [datetime.strptime(item, '%Y-%m-%d') for item in date_published]
+    result_df = DataFrame(data={'id': video_ids, 'views': views, 'published': date_published})
+    result_df['published'] = to_datetime(arg=result_df['published'], )
+    result_df['views'] = result_df['views'].astype(int)
+    result_df =  result_df.sort_values(by='published', )
     return result_df
-
-
-def get_timedelta(adjustment: str):
-    pieces = adjustment.split()
-    amount = -int(pieces[0])
-    if pieces[1] == 'hours':
-        return timedelta(hours=amount)
-    elif pieces[1] == 'days':
-        return timedelta(days=amount)
-    elif pieces[1] == 'months':
-        return timedelta(days=30 * amount)
-    elif pieces[1] == 'years':
-        return timedelta(days=365 * amount)
-    else:
-        return timedelta(days=0)
 
 
 def main():
@@ -76,8 +68,9 @@ def main():
 
     videos_generator = get_generator(channel=settings['channel_url'], channel_kind='url', )
     videos_df = get_data_from_generator(videos=videos_generator, )
+    videos_df.to_csv(index=False, path_or_buf='./videos.csv', )
 
-    scatterplot(data=videos_df, x='published_date', y='view_count', )
+    scatterplot(data=videos_df, x='published', y='views', )
     filename = OUTPUT_FOLDER + FILENAME
     logger.info(msg='saving plot to {}'.format(filename))
     savefig(backend=None, bbox_inches=None, dpi='figure', edgecolor='auto', facecolor='auto', fname=filename,
