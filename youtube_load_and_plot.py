573e972b7dc85b8a3f00e88049cc3cfc8b601972
Mike DeLong <mikedelong@outlook.com>
2023-05-31 11:26:07 -0400

    refactor: define USECOLS at file scope; also, factor out the plotting into a separate function

--- youtube_load_and_plot.py
+++ youtube_load_and_plot.py
@@ -8,10 +8,42 @@ from matplotlib.pyplot import close
 from matplotlib.pyplot import savefig
 from matplotlib.pyplot import scatter
 from matplotlib.pyplot import ylabel
+from pandas import DataFrame
 from pandas import read_csv
 from pandas import to_datetime
-from seaborn import scatterplot
 from plotly.express import scatter as plotly_scatter
+from seaborn import scatterplot
+
+
+def make_plot(plotting_package: str, df: DataFrame, ):
+    logger = getLogger(name='make_plot', )
+    if plotting_package not in {'matplotlib.pyplot', 'plotly', 'seaborn'}:
+        raise NotImplementedError(plotting_package)
+    elif plotting_package == 'matplotlib.pyplot':
+        filename = OUTPUT_FOLDER + SCATTER_FILENAME
+        scatter(data=df, x='published', y='log10_views', )
+        ylabel(ylabel='log10 views')
+        logger.info(msg='saving plot to {}'.format(filename), )
+        savefig(backend=None, bbox_inches=None, dpi='figure', edgecolor='auto', facecolor='auto', fname=filename,
+                format='png', metadata=None, pad_inches=0.1, )
+        close()
+    elif plotting_package == 'plotly':
+        filename = OUTPUT_FOLDER + SCATTER_PLOTLY_FILENAME
+        figure = plotly_scatter(data_frame=df, x='published', y='log10_views',
+                                title='Title', labels={'published': 'Date Published', 'log10_views': 'log10 of views'})
+        logger.info(msg='saving plot to {}'.format(filename), )
+        figure.write_html(filename)
+    elif plotting_package == 'seaborn':
+        filename = OUTPUT_FOLDER + SCATTERPLOT_FILENAME
+        scatterplot(data=df, x='published', y='log10_views', )
+        ylabel(ylabel='log10 views')
+        logger.info(msg='saving plot to {}'.format(filename), )
+        savefig(backend=None, bbox_inches=None, dpi='figure', edgecolor='auto', facecolor='auto', fname=filename,
+                format='png', metadata=None, pad_inches=0.1, )
+        close()
+    else:
+        raise NotImplementedError(plotting_package)
+
 
 def main():
     time_start = now()
@@ -21,36 +53,11 @@ def main():
     with open(encoding='utf-8', file='youtube_load_and_plot.json', mode='r') as input_fp:
         settings = load(fp=input_fp, )
 
-    videos_df = read_csv(filepath_or_buffer=settings['input_data_file'], usecols=['published', 'log10_views'])
+    videos_df = read_csv(filepath_or_buffer=settings['input_data_file'], usecols=USECOLS, )
     videos_df['published'] = to_datetime(arg=videos_df['published'], )
     videos_df = videos_df.sort_values(by='published', )
     plotting_package = settings['plotting_package']
-    if plotting_package not in {'matplotlib.pyplot', 'plotly', 'seaborn'}:
-        raise NotImplementedError(plotting_package)
-    elif plotting_package == 'matplotlib.pyplot':
-        filename = OUTPUT_FOLDER + SCATTER_FILENAME
-        scatter(data=videos_df, x='published', y='log10_views', )
-        ylabel(ylabel='log10 views')
-        logger.info(msg='saving plot to {}'.format(filename), )
-        savefig(backend=None, bbox_inches=None, dpi='figure', edgecolor='auto', facecolor='auto', fname=filename,
-                format='png', metadata=None, pad_inches=0.1, )
-        close()
-    elif plotting_package == 'plotly':
-        filename = OUTPUT_FOLDER + SCATTER_PLOTLY_FILENAME
-        figure = plotly_scatter(data_frame=videos_df, x='published', y='log10_views',
-                                title= 'Title', labels={'published': 'Date Published', 'log10_views': 'log10 of views'})
-        logger.info(msg='saving plot to {}'.format(filename), )
-        figure.write_html(filename)
-    elif plotting_package == 'seaborn':
-        filename = OUTPUT_FOLDER + SCATTERPLOT_FILENAME
-        scatterplot(data=videos_df, x='published', y='log10_views', )
-        ylabel(ylabel='log10 views')
-        logger.info(msg='saving plot to {}'.format(filename), )
-        savefig(backend=None, bbox_inches=None, dpi='figure', edgecolor='auto', facecolor='auto', fname=filename,
-                format='png', metadata=None, pad_inches=0.1, )
-        close()
-    else:
-        raise NotImplementedError(plotting_package)
+    make_plot(df=videos_df, plotting_package=plotting_package, )
 
     time_seconds = (now() - time_start).total_seconds()
     logger.info(msg='done: {:02d}:{:05.2f}'.format(int(time_seconds // 60), time_seconds % 60, ))
@@ -60,6 +67,7 @@ OUTPUT_FOLDER = './result/'
 SCATTER_FILENAME = 'youtube.matplotlib.scatter.png'
 SCATTER_PLOTLY_FILENAME = 'youtube.plotly.scatter.html'
 SCATTERPLOT_FILENAME = 'youtube.seaborn.scatterplot.png'
+USECOLS = ['published', 'log10_views']
 
 if __name__ == '__main__':
     main()
